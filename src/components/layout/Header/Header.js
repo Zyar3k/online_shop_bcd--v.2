c514adf3f93fd0db261e87160cff1464d79e11dd
Arkadiusz Zygan <a.zygan@gmail.com>
2021-03-19 14:36:52 +0100

    Add mobile view to header

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -8,14 +8,12 @@ const Header = () => {
 
   console.log(isOpen);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-    console.log("toggle");
-  };
+  const mobileMenuToggle = () => setIsOpen(!isOpen);
+  const closeMobileMenu = () => setIsOpen(false);
   return (
     <header className={styles.header}>
       <nav className={`${styles.navBar} mainLayContainer`}>
-        <NavLink to='/' className={`${styles.navItem} ${styles.logo}`}>
+        <NavLink to='/' className={styles.logo}>
           <span>B</span>
           etty
           <span>C</span>
@@ -23,19 +21,34 @@ const Header = () => {
           <span>D</span>
           esigns
         </NavLink>
-
-        <NavLink className={styles.navItem} to='/'>
-          Home
-        </NavLink>
-        <NavLink className={styles.navItem} to='/contact'>
-          Contact
-        </NavLink>
-        <NavLink className={styles.navItem} to='/cart'>
-          Cart
-        </NavLink>
+        <div
+          className={
+            isOpen
+              ? `${styles.navItemWrapper} ${styles.active}`
+              : `${styles.navItemWrapper}`
+          }
+        >
+          <NavLink onClick={closeMobileMenu} className={styles.navItem} to='/'>
+            Home
+          </NavLink>
+          <NavLink
+            onClick={closeMobileMenu}
+            className={styles.navItem}
+            to='/contact'
+          >
+            Contact
+          </NavLink>
+          <NavLink
+            onClick={closeMobileMenu}
+            className={styles.navItem}
+            to='/cart'
+          >
+            Cart
+          </NavLink>
+        </div>
         <div className={styles.toggleMenuWrapper}>
           <i
-            onClick={toggleMenu}
+            onClick={mobileMenuToggle}
             className={isOpen ? "fas fa-times" : "fas fa-bars"}
           ></i>
         </div>
--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -5,6 +5,54 @@ class Homepage extends Component {
     return (
       <div>
         <p>Homepage</p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam
+          inventore iure, ducimus a ipsa atque recusandae beatae saepe
+          voluptatibus nam quidem omnis nemo deleniti ab quod veniam. Modi velit
+          rerum, cum culpa, tenetur optio impedit repellendus sequi fuga
+          pariatur praesentium vitae minima iusto esse. Eligendi, molestias.
+          Facilis dolore voluptas dolorum, ut fugit ipsum quod architecto
+          voluptate itaque temporibus, nobis quisquam, velit ipsa magni quo
+          officia suscipit voluptatem sint. Porro soluta ipsam exercitationem?
+          Ullam ut eligendi, quibusdam magnam dolor distinctio error facere iste
+          quisquam exercitationem qui ipsa assumenda deleniti at aspernatur
+          iusto? Corporis facere inventore fuga eaque minima libero ipsum atque
+          quam, magni esse delectus doloremque. Veritatis nulla, autem ab ut
+          magni maxime impedit nostrum voluptates ratione molestiae natus, vitae
+          soluta fuga pariatur aliquid quod reprehenderit qui quisquam est
+          atque, nesciunt blanditiis at delectus quo! Ab saepe illo sit fugiat
+          maiores officiis tempore, error natus qui quisquam sequi, nisi hic nam
+          eius enim numquam aliquid earum. Vitae cupiditate facilis natus
+          quaerat, blanditiis veniam cum omnis dignissimos repellat, ipsa minus
+          at iusto reprehenderit deserunt quod, eos numquam id. Quod ab in odio
+          voluptatum saepe officiis, mollitia asperiores eligendi vero et fugit
+          veritatis iusto minus! Maxime assumenda temporibus officiis labore
+          numquam consequuntur fugit.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam
+          inventore iure, ducimus a ipsa atque recusandae beatae saepe
+          voluptatibus nam quidem omnis nemo deleniti ab quod veniam. Modi velit
+          rerum, cum culpa, tenetur optio impedit repellendus sequi fuga
+          pariatur praesentium vitae minima iusto esse. Eligendi, molestias.
+          Facilis dolore voluptas dolorum, ut fugit ipsum quod architecto
+          voluptate itaque temporibus, nobis quisquam, velit ipsa magni quo
+          officia suscipit voluptatem sint. Porro soluta ipsam exercitationem?
+          Ullam ut eligendi, quibusdam magnam dolor distinctio error facere iste
+          quisquam exercitationem qui ipsa assumenda deleniti at aspernatur
+          iusto? Corporis facere inventore fuga eaque minima libero ipsum atque
+          quam, magni esse delectus doloremque. Veritatis nulla, autem ab ut
+          magni maxime impedit nostrum voluptates ratione molestiae natus, vitae
+          soluta fuga pariatur aliquid quod reprehenderit qui quisquam est
+          atque, nesciunt blanditiis at delectus quo! Ab saepe illo sit fugiat
+          maiores officiis tempore, error natus qui quisquam sequi, nisi hic nam
+          eius enim numquam aliquid earum. Vitae cupiditate facilis natus
+          quaerat, blanditiis veniam cum omnis dignissimos repellat, ipsa minus
+          at iusto reprehenderit deserunt quod, eos numquam id. Quod ab in odio
+          voluptatum saepe officiis, mollitia asperiores eligendi vero et fugit
+          veritatis iusto minus! Maxime assumenda temporibus officiis labore
+          numquam consequuntur fugit.
+        </p>
       </div>
     );
   }
