afce7a7281dd8a2fcdae5fff01b4e9036377a606
Arkadiusz Zygan <a.zygan@gmail.com>
2021-03-24 15:17:06 +0100

    Init reducer and store

---
+++ src/redux/productRedux.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { API_URL } from "../config";
+
+const reducerName = "products";
+const createActionName = (name) => `app/${reducerName}/${name}`;
+
+export const getProducts = ({ products }) => products.data;
+
+export const LOAD_PRODUCTS = createActionName("LOAD_PRODUCTS");
+
+export const loadProducts = (payload) => ({ payload, type: LOAD_PRODUCTS });
+
+const initialState = { data: [] };
+
+export const loadProductsRequest = () => {
+  return async (dispatch) => {
+    try {
+      let res = await axios.get(`${API_URL}/products`);
+      dispatch(loadProducts(res.data));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+};
+
+export default function reducer(statePart = initialState, action = {}) {
+  switch (action.type) {
+    /* product */
+    case LOAD_PRODUCTS:
+      return { ...statePart, data: action.payload };
+    default:
+      return statePart;
+  }
+}
